6e9b760b7777fd5aba21dd0a8385b2396ec6cf1c
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-10-15 10:35:27 +0700

    Chapter 9: Advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,28 @@
 module SessionsHelper
-  def log_in user
+  def log_in user, remember_me
     # saves user id to client's session cookie
     session[:user_id] = user.id
+    remember user if remember_me == "1"
+  end
+
+  def remember user
+    user.remember_me
+    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
   end
 
   def current_user
     # gets current user info
-    @current_user ||= User.find_by id: session[:user_id]
+    # @current_user ||= User.find_by id: session[:user_id]
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by id: user_id
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by id: user_id
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user, false
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -16,6 +32,7 @@ module SessionsHelper
 
   def log_out
     session.delete(:user_id)
+    cookies.delete :user_id if cookies.signed[:user_id]
     @current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,29 @@ class User < ApplicationRecord
   validates :password, presence: true,
             length: {minimum: Settings.max_length.password_8}
   has_secure_password
+  attr_accessor :remember_token
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
+  def remember_me
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
 end
