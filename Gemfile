37d464c3ccf70b1ca12dacd38e5e2a119ca02dee
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-10-15 10:35:27 +0700

    Chapter 7: Sign up
    added i18n
    
    Chapter 6: Modeling user
    
    Chapter 7: Sign up

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem "jbuilder", "~> 2.7"
 gem "bootstrap-sass", "3.4.1"
 gem "rails-i18n", "~> 6.0.0"
 gem "config"
+gem "bcrypt", "3.1.13"
 
 gem "bootsnap", ">= 1.4.4", require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/home", to: "static_pages#home"
   get "/help", to: "static_pages#help"
+  get "/register", to: "users#new"
+  post "/register", to: "users#create"
+  resources :users, only: %i(create new show)
 end
